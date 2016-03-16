f37d82c467b01eae728323e062dfb6bc6a53af78
Redouan <0882305@hr.nl>
2016-03-16 12:12:37 +0100

    Update dbconnect.php
    Updated connection details

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,9 +1,9 @@
 
 <?php
-    $host = "";
-    $database = "";
-    $user = "";
-    $password = "";
+    $host = "https://sql.hosted.hro.nl/";
+    $database = "prj_2015_2016_clemob_mt1bd_e";
+    $user = "prj_2015_2016_clemob_mt1bd_e";
+    $password = "chaepahj";
     $prefix = "";
 
     $db = mysqli_connect($host, $user, $password, $database) or die("Error: " . mysqli_connect_error());;
