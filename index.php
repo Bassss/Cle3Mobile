00cb19f55036c0384b6f2e50cb61726888f2b556
Redouan <0882305@hr.nl>
2016-03-16 14:39:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,12 @@
 
 include_once($_SERVER["DOCUMENT_ROOT"] . "/includes/dbconnect.php");
 
-$sql = "SELECT * FROM messages";
-$rs = mysqli_query($link, $sql);
+$sql = "SELECT * FROM `messages`";
+$rs = mysqli_query($db, $sql);
+if(mysqli_num_rows($rs) > 0)
+{
+  while($row = mysqli_fetch_assoc($rs))
+  {
+      echo $row["content"];
+  }
+}
