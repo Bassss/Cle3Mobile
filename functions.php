7456de4d69398b7e0a1054efd426d92e555f13fd
Redouan <0882305@hr.nl>
2016-03-21 11:16:41 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,70 @@
+<?php
+
+function getMessageType($type)
+{
+    switch ($type) {
+        case 0: {
+            $type = "Message";
+            break;
+        }
+        case 1: {
+            $type = "Spotify";
+            break;
+        }
+        case 2: {
+            $type = "Image";
+            break;
+        }
+        case 3: {
+            $type = "Youtube";
+            break;
+        }
+    }
+    return $type;
+}
+
+function getMessageColor($color)
+{
+    switch ($color) {
+        case 0: {
+            $color = "Red";
+            break;
+        }
+        case 1: {
+            $color = "Green";
+            break;
+        }
+        case 2: {
+            $color = "Blue";
+            break;
+        }
+        case 3: {
+            $color = "Cyan";
+            break;
+        }
+        case 4: {
+            $color = "Yellow";
+            break;
+        }
+    }
+    return $color;
+}
+
+function getMessageStatus($status)
+{
+    switch ($status) {
+        case 0: {
+            $status = "New & unread";
+            break;
+        }
+        case 1: {
+            $status = "Loaded, ready to be shown";
+            break;
+        }
+        case 2: {
+            $status = "Shown";
+            break;
+        }
+    }
+    return $status;
+}
