7a7ac0e7ce0ed18ec507f6b1dd81d8ab465da572
Maaike Koolmees <0911174@hr.nl>
2016-03-21 16:05:49 +0100

    Mobile test 2, photo upload finished

--- index.php
+++ index.php
@@ -1,77 +1,37 @@
-<?php
-include_once("includes/dbconnect.php");
-include_once("includes/functions.php");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lama</title>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/icons.css">
+    <link rel="stylesheet" href="plugins/fontawesome/css/font-awesome.min.css">
+    <script src="plugins/js/modernizr.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body data-hijacking="on" data-animation="rotate">
 
-if (isset($_POST["submit"])) {
-    $content = mysqli_real_escape_string($db, $_POST["content"]);
-    $type = mysqli_real_escape_string($db, $_POST["type"]);
-    $desc = mysqli_real_escape_string($db, $_POST["description"]);
-    $content = str_replace("watch?v=", "embed/", $content);
-    $curdate = time();
-    $sql = "INSERT INTO `messages` (`type`, `content`, `description`, `date`) VALUES ('$type','$content','$desc', '$curdate')";
-    $rs = mysqli_query($db, $sql);
-}
-?>
-<h2>Let's <s>ch</s>shair!</h2>
-<table style="border: 1px solid black;" cellpadding="5">
-    <thead>
-    <tr>
-        <th>Type</th>
-        <th>Content</th>
-        <th>Description</th>
-        <th>Color</th>
-        <th>Date</th>
-        <th>Status</th>
-    </tr>
-    </thead>
-    <?php
+<div id="content">
+<?php include("home.html"); ?>
+</div>
 
-    $sql = "SELECT * FROM `messages` ORDER BY `date` DESC";
-    $rs = mysqli_query($db, $sql);
-    if (mysqli_num_rows($rs) > 0) {
-        while ($row = mysqli_fetch_assoc($rs)) {
-            ?>
-            <tr>
-                <td><?= getMessageType($row["type"]); ?></td>
-                <td>
-                    <?php
-                    if (getMessageType($row["type"]) == "Youtube") {
-                        ?>
-                        <iframe width="250" height="150" src="<?= $row["content"]; ?>" frameborder="0" allowfullscreen></iframe>
-                        <?php
-                    } else if (getMessageType($row["type"]) == "Image") {
-                        ?>
-                        <img height="100px" src="<?= $row["content"]; ?>">
-                        <?php
-                    } else if (getMessageType($row["type"]) == "Spotify") {
-                        ?>
-                        <iframe src="https://embed.spotify.com/?uri=<?= $row["content"]; ?>" frameborder="0" allowtransparency="true"></iframe>
-                        <?php
+<div class="nav">
+    <ul>
+        <li><a href="camera"><i id="camera" class="fa fa-camera fa-3x"></i></a></li>
+        <li><a href="text"><i id="text" class="fa fa-file-text fa-3x"></i></a></li>
+        <li><a href="quote"><i id="quote" class="fa fa-comment fa-3x"></i></a></li>
+        <li><a href="music"><i id="music" class="fa fa-music fa-3x"></i></a></li>
+    </ul>
+</div>
 
-                    } else {
-                        ?>
-                        <?= $row["content"]; ?>
-                        <?php
-                    }
-                    ?>
-                </td>
-                <td><?= $row["description"]; ?></td>
-                <td><?= getMessageColor($row["color"]); ?></td>
-                <td><?= date("d-M-Y H:i", $row["date"]); ?></td>
-                <td><?= getMessageStatus($row["status"]); ?></td>
-            </tr>
-            <?php
-        }
-    }
-    ?>
-</table>
-<hr>
-<form action="index.php" method="POST" name="addMessage">
-    <input type="text" name="content" placeholder="Content"><br>
-    <input type="radio" name="type" value="0">Quote<br>
-    <input type="radio" name="type" value="1">Spotify<br>
-    <input type="radio" name="type" value="2">Image<br>
-    <input type="radio" name="type" value="3">Youtube video<br>
-    <textarea name="description" placeholder="Description"></textarea>
-    <input type="submit" name="submit" value="Add my message">
-</form>
+
+    <!-- Scripts -->
+    <script src="plugins/js/jquery-2.1.4.js"></script>
+    <script src="js/load.js"></script>
+    <script src="plugins/js/velocity.min.js"></script>
+    <script src="plugins/js/velocity.ui.min.js"></script>
+    <script src="plugins/js/main.js"></script>
+    <script src="js/scroll.js"></script>
+</body>
+</html>
--- js/load.js
+++ js/load.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
     //Initial
-    $('#content').load('home.html');
+    //$('#content').load('home.html');
 
     //Menu click handler
     $('.nav ul li a').click(function() {
+        var page = $(this).attr('href');
+        $('#content').load(page + '.html');
+        return false;
+    });
+
+    $('.page-nav a').click(function() {
         var page = $(this).attr('href');
         $('#content').load(page + '.html');
         return false;
