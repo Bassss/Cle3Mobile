2e641603c91ce7972a90768cb096c4220ed26de4
Bas Schoenmaker <0910922@hr.nl>
2016-03-30 13:58:41 +0200

    dubble RGB

--- Arduino/index.js
+++ Arduino/index.js
@@ -1,14 +1,14 @@
 var five = require('johnny-five');
 
 var led;
-var r = 0;
-var b = 0;
-var g = 0;
+var r = 1;
+var b = 1;
+var g = 1;
 
 var led2;
-var r2 = 0;
-var b2 = 0;
-var g2 = 0;
+var r2 = 1;
+var b2 = 1;
+var g2 = 1;
 
 five.Board().on('ready', function() {
   console.log('ready');
@@ -16,8 +16,8 @@ five.Board().on('ready', function() {
   // Initialize the RGB LED
   led = new five.Led.RGB({
     pins: {
-      red: 6,
-      green: 5,
+      red: 5,
+      green: 6,
       blue: 3
     }
   });
@@ -29,22 +29,23 @@ five.Board().on('ready', function() {
 
 // Initialize the RGB LED2
 led2 = new five.Led.RGB({
-  pins2: {
-    red2: 11,
-    green2: 10,
-    blue2: 9
+  pins: {
+    red: 9,
+    green: 10,
+    blue: 11
   }
 });
 
 // Turn it on and set the initial color
 led2.on();
-led2.color({red2: r2, blue2: b2, green2: g2});
+led2.color({red: r2, blue: b2, green: g2});
 });
 var pubnub = require('pubnub').init({
   subscribe_key: 'sub-c-74c85f44-f100-11e5-8112-02ee2ddab7fe',
   publish_key:   'pub-c-6ec1bade-3025-42e6-b4f7-a953fee47d4c'
 });
-var channel = 'Channel-u7cd341a3';
+var channel = 'Channel-zq7jdibrr';
+
 
 pubnub.subscribe({
   channel: channel,
@@ -58,6 +59,16 @@ pubnub.subscribe({
 
       console.log( 'color change to...' );
       console.log( led.color() );
+    }
+    if(led2) {
+      r2 = (m.color === 'red2') ? m.brightness : r2;
+      g2 = (m.color === 'green2') ? m.brightness : g2;
+      b2 = (m.color === 'blue2') ? m.brightness : b2;
+
+      led2.color({red: r2, blue: b2, green: g2});
+
+      console.log( 'color2 change to...' );
+      console.log( led2.color() );
     }
   },
   error: function(err) {console.log(err);}
