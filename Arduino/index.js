599c3cc281cdbf587bf6a7c391f4a713bbf29c9e
Bas Schoenmaker <0910922@hr.nl>
2016-03-31 11:31:38 +0200

    dubble led thingy

--- Arduino/index.js
+++ Arduino/index.js
@@ -1,20 +1,17 @@
 var five = require('johnny-five');
-
 var led;
+var led1;
+var led2;
 var r = 1;
 var b = 1;
 var g = 1;
-
-var led2;
-var r2 = 1;
-var b2 = 1;
-var g2 = 1;
+var chair;
 
 five.Board().on('ready', function() {
   console.log('ready');
 
   // Initialize the RGB LED
-  led = new five.Led.RGB({
+  led1 = new five.Led.RGB({
     pins: {
       red: 5,
       green: 6,
@@ -23,8 +20,8 @@ five.Board().on('ready', function() {
   });
 
   // Turn it on and set the initial color
-  led.on();
-  led.color({red: r, blue: b, green: g});
+  led1.on();
+  led1.color({red: r, blue: b, green: g});
 
 
 // Initialize the RGB LED2
@@ -38,8 +35,10 @@ led2 = new five.Led.RGB({
 
 // Turn it on and set the initial color
 led2.on();
-led2.color({red: r2, blue: b2, green: g2});
+led2.color({red: r, blue: b, green: g});
 });
+
+
 var pubnub = require('pubnub').init({
   subscribe_key: 'sub-c-74c85f44-f100-11e5-8112-02ee2ddab7fe',
   publish_key:   'pub-c-6ec1bade-3025-42e6-b4f7-a953fee47d4c'
@@ -50,25 +49,22 @@ var channel = 'Channel-zq7jdibrr';
 pubnub.subscribe({
   channel: channel,
   callback: function(m) {
+    chair = m.chair;
+    if (chair == 1){
+      led = led1;
+    } else {
+      led = led2;
+    }
     if(led) {
-      r = (m.color === 'red') ? m.brightness : r;
-      g = (m.color === 'green') ? m.brightness : g;
-      b = (m.color === 'blue') ? m.brightness : b;
+      r = m.red;
+      g = m.green;
+      b = m.blue;
+
 
       led.color({red: r, blue: b, green: g});
 
-      console.log( 'color change to...' );
+      console.log( chair, 'color change to...' );
       console.log( led.color() );
-    }
-    if(led2) {
-      r2 = (m.color === 'red2') ? m.brightness : r2;
-      g2 = (m.color === 'green2') ? m.brightness : g2;
-      b2 = (m.color === 'blue2') ? m.brightness : b2;
-
-      led2.color({red: r2, blue: b2, green: g2});
-
-      console.log( 'color2 change to...' );
-      console.log( led2.color() );
     }
   },
   error: function(err) {console.log(err);}
